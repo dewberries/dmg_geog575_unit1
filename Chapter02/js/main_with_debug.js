1572ca9fa794a2c71e89742a49a4f3bd7625aa24
dewberries <158351105+dewberries@users.noreply.github.com>
2025-10-07 20:31:43 -0500

    debug5

--- Chapter02/js/main_with_debug.js
+++ Chapter02/js/main_with_debug.js
@@ -1,90 +1,57 @@
-//initialize function called when the script loads
-function initialize(){
-    addColumns(cityPop);
-	addEvents();
-};
-
-
-
 var cityPop = [
-	{ 
-		city: 'Madison',
-		population: 233209
-	},
-	{
-		city: 'Milwaukee',
-		population: 594833
-	},
-	{
-		city: 'Green Bay',
-		population: 104057
-	},
-	{
-		city: 'Superior',
-		population: 27244
-	}
+	{ city: 'Madison', population: 233209 },
+	{ city: 'Milwaukee', population: 594833 },
+	{ city: 'Green Bay', population: 104057 },
+	{ city: 'Superior', population: 27244 }
 ];
 
-//create a table element
-var table = document.createElement("table");
-document.querySelector("#mydiv").appendChild(table);
-	
-
 function addColumns(cityPop){
-    
-    document.querySelectorAll("tr").forEach(function(row, i){
+	document.querySelectorAll("tr").forEach(function(row, i){
 
-    	if (i == 0){
+		if (i == 0){
+			// fixed typo: insertAdjacntHTML → insertAdjacentHTML
+			row.insertAdjacentHTML('beforeend', '<th>City Size</th>');
+		} else {
+			var citySize;
 
-    		row.insertAdjacentHTML('beforeend', '<th>City Size</th>');
-    	} else {
+			if (cityPop[i-1].population < 100000){
+				citySize = 'Small';
+			} else if (cityPop[i-1].population < 500000){
+				citySize = 'Medium'; // fixed variable name typo: citysize → citySize
+			} else {
+				citySize = 'Large';
+			};
 
-    		var citySize;
-
-    		if (cityPop[i-1].population < 100000){
-    			citySize = 'Small';
-
-    		} else if (cityPop[i-1].population < 500000){
-    			citySize = 'Medium';
-
-    		} else {
-    			citySize = 'Large';
-    		};
-
+			// fixed syntax: use insertAdjacentHTML instead of assignment
 			row.insertAdjacentHTML('beforeend', '<td>' + citySize + '</td>');
-    	}
-    });
-}
+		};
+	});
+};
 
 function addEvents(){
+	var table = document.querySelector("table");
 
-	document.querySelector("table").addEventListener("mouseover", function() {
-		
+	table.addEventListener("mouseover", function(){
 		var color = "rgb(";
 
-		for (var i=0; i<3; i++){
-
+		for (var i = 0; i < 3; i++){
 			var random = Math.round(Math.random() * 255);
+			color += random; // removed quotes around random
 
-			color += random;
-
-			if (i<2){
+			if (i < 2){
 				color += ",";
-			
 			} else {
 				color += ")";
 			}
 		}
 
-		document.querySelector("table").style.color = color;
+		// fixed property: .style.color instead of .color
+		table.style.color = color;
 	});
 
 	function clickme(){
-
 		alert('Hey, you clicked me!');
 	};
 
-	document.querySelector("table").addEventListener("click", clickme);
-}
-
-document.addEventListener('DOMContentLoaded',initialize);
+	table.addEventListener("click", clickme);
+};
